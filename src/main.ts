e4ab8cae104ba1b3b5ea82f90a781fdf9574c7d0
SelimJB <selim.bennani@gmail.com>
2024-06-13 17:02:16 +0700

    Phaser Hello World & Configuration

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import Phaser from 'phaser';
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: 'game-container',
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+const game = new Phaser.Game(config);
+
+function preload(this: Phaser.Scene) {
+  this.load.image(
+    'clouds',
+    'assets/clouds.png'
+  );
+}
+
+function create(this: Phaser.Scene) {
+  this.add.image(400, 300, 'clouds');
+  this.add.text(400, 500, 'Hello !', { fontSize: '70px', color: '#fff' }).setOrigin(0.5);
+}
+
+function update(this: Phaser.Scene) {}
