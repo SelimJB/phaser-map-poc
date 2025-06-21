33aae65b723b6e806e070e4dc9d1f25c54c13cf6
SelimJB <selim.bennani@gmail.com>
2025-06-21 20:27:19 +0100

    Update configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,20 @@ export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
   base: '/',
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx'],
+    alias: {
+      '@': '/src',
+      '@phaser': '/src/phaser',
+      '@map': '/src/phaser/map'
+    }
+  },
   build: {
     outDir: 'dist'
   },
   server: {
     port: 3000,
     watch: {}
-  }
+  },
+  assetsInclude: ['**/*.frag']
 });
